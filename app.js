00723f68235f1c6bb1950946ac9bf9bdc185d053
jason <101104044@nccu.edu.tw>
2018-08-01 18:10:26 +0800

    v2 線條version

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var width = window.innerWidth;
 var height = window.innerHeight - 25;
+var first = true;
+var tempPos = [];
+var savePos = [];
+var lastPos;
 // first we need Konva core things: stage and layer
 var stage = new Konva.Stage({
   container: "container",
@@ -41,31 +45,62 @@ stage.on("contentMouseup.proto", function() {
   isPaint = false;
 });
 // and core function - drawing
-stage.on("contentMousemove.proto", function() {
-  if (!isPaint) {
-    return;
-  }
-  if (mode === "brush") {
-    context.globalCompositeOperation = "source-over";
-  }
-  if (mode === "eraser") {
-    context.globalCompositeOperation = "destination-out";
-  }
-  context.beginPath();
-  var localPos = {
-    x: lastPointerPosition.x - image.x(),
-    y: lastPointerPosition.y - image.y()
-  };
-  context.moveTo(localPos.x, localPos.y);
+// stage.on("contentMousemove.proto", function() {
+stage.on("contentClick.proto", function() {
+  // 基本座標資訊
   var pos = stage.getPointerPosition();
   localPos = {
     x: pos.x - image.x(),
     y: pos.y - image.y()
   };
+  context.globalCompositeOperation = "source-over";
+  if (tempPos.length > 1) {
+    let x0 = tempPos[0].x;
+    let y0 = tempPos[0].y;
+    console.log(x0, y0);
+    let x_square = (x0 - localPos.x) * (x0 - localPos.x);
+    let y_square = (y0 - localPos.y) * (y0 - localPos.y);
+    let length = Math.sqrt(x_square + y_square);
+    if (length < 25) {
+      let points = [];
+      tempPos.forEach(e => {
+        points.push(e.x);
+        points.push(e.y);
+      });
+      var poly = new Konva.Line({
+        points: points,
+        fill: "#00D2FF",
+        stroke: "black",
+        strokeWidth: 5,
+        closed: true
+      });
+      // add the shape to the layer
+      layer.add(poly);
+      stage.add(layer);
+      first = true;
+    }
+  }
+
+  context.beginPath();
+  var localPos = {
+    x: lastPointerPosition.x - image.x(),
+    y: lastPointerPosition.y - image.y()
+  };
+  if (first) {
+    console.log('first')
+    context.moveTo(localPos.x, localPos.y);
+    first = false;
+  } else {
+    context.moveTo(lastPos.x, lastPos.y);
+  }
+
   context.lineTo(localPos.x, localPos.y);
+  tempPos.push({ x: localPos.x, y: localPos.y });
   context.closePath();
   context.stroke();
   lastPointerPosition = pos;
+  lastPos = localPos;
+  console.log(tempPos);
   layer.draw();
 });
 var select = document.getElementById("tool");
