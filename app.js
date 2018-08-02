594188fbc582adbf06e9bfd9a8a8d6a242b5c3d8
jason <101104044@nccu.edu.tw>
2018-08-02 16:12:47 +0800

    drag group version

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 // 模擬一開始server下來的資料
-var serverData=[
-    {
-        name:'a1',
-        points:[1737, 166, 1736, 258, 1660, 268, 1688, 150, 1735, 167]
-    },{
-        name:'a2',
-        points:[1253, 134, 1164, 202, 1294, 232, 1256, 136]
-    },{
-        name:'a3',
-        points:[559, 263, 537, 233, 446, 220, 403, 279, 395, 349, 458, 423, 572, 438, 610, 401, 620, 344, 564, 268]
-    }
-]
-// console.log(serverData)
-// serverData.forEach(e=>{
-//     console.log(e)
-// })
-// server end
+var serverData = [
+  {
+    name: "a1",
+    points: [1737, 166, 1736, 258, 1660, 268, 1688, 150, 1735, 167]
+  },
+  {
+    name: "a2",
+    points: [1253, 134, 1164, 202, 1294, 232, 1256, 136]
+  },
+  {
+    name: "a3",
+    points: [
+      559,
+      263,
+      537,
+      233,
+      446,
+      220,
+      403,
+      279,
+      395,
+      349,
+      458,
+      423,
+      572,
+      438,
+      610,
+      401,
+      620,
+      344,
+      564,
+      268
+    ]
+  }
+];
 var width = window.innerWidth;
 var height = window.innerHeight;
 var stage = new Konva.Stage({
@@ -31,28 +49,48 @@ var clickPoints = [];
 var rectPonits = [];
 var layer = new Konva.Layer();
 // ajax data start
-serverData.forEach((e,i)=>{
-    var rect = new Konva.Line({
-        points:e.points,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 5,
-        closed: true,
-        draggable: true,
-    });
-    var rectText = new Konva.Text({
-        x: e.points[0],
-        y: e.points[1],
-        text: e.name,
-        fontSize: 30,
-        fontFamily: 'Calibri',
-        fill: 'green',
-        draggable:true
-      });
-    layer.add(rect);
-    layer.add(rectText)
-    stage.add(layer);
-})
+
+serverData.forEach((e, i) => {
+  var group = new Konva.Group({
+    draggable: true
+  });
+  var SrvRect = new Konva.Line({
+    points: e.points,
+    fill: "red",
+    stroke: "black",
+    strokeWidth: 5,
+    closed: true,
+    // draggable: true,
+    Opacity: 0.6
+  });
+  var SrvRectText = new Konva.Text({
+    x: e.points[0],
+    y: e.points[1],
+    text: e.name,
+    fontSize: 30,
+    fontFamily: "Calibri",
+    fill: "green"
+    // draggable:true
+  });
+  group.add(SrvRect);
+  group.add(SrvRectText);
+  layer.add(group);
+  group.on("mouseover", function() {
+    document.body.style.cursor = "pointer";
+  });
+  group.on("mouseout", function() {
+    document.body.style.cursor = "default";
+  });
+  // layer.add(SrvRect);
+  // layer.add(SrvRectText)
+  stage.add(layer);
+  group.on("dragend", function(e) {
+    console.log(group)
+    console.log(e.evt.dragEndNode.attrs.x);
+    console.log(e.evt.dragEndNode.attrs.y);
+  });
+});
+
 // ajax data end
 // dashed line
 var tempLine = new Konva.Line({
@@ -63,8 +101,8 @@ var tempLine = new Konva.Line({
   dash: [33, 10]
 });
 var rect = new Konva.Line({
-//   points: rectPonits[0],
-  points:clickPoints,
+  //   points: rectPonits[0],
+  points: clickPoints,
   fill: "red",
   stroke: "black",
   strokeWidth: 5,
@@ -87,13 +125,13 @@ stage.on("contentClick", e => {
     let length = Math.sqrt(x_square + y_square);
     // 距離小於25，點連成線，大於25繼續畫點
     if (length < 25) {
-        console.log('asd')
-    //   rectPonits.push(clickPoints);
-    //   console.log(rectPonits)
-    //   clickPoints=[]
-        console.log(clickPoints)
+      console.log("asd");
+      //   rectPonits.push(clickPoints);
+      //   console.log(rectPonits)
+      //   clickPoints=[]
+      console.log(clickPoints);
       layer.add(rect);
-    //   layer.add(tempLine)
+      //   layer.add(tempLine)
       stage.add(layer);
     } else {
       layer.add(tempLine);
@@ -102,6 +140,7 @@ stage.on("contentClick", e => {
     // console.log("要算距離囉");
   }
 });
+
 // let x0 = tempPos[0].x;
 // let y0 = tempPos[0].y;
 // console.log(x0, y0);
